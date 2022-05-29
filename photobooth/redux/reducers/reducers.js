5a28dcd794db95d2fca5ebb7d02c1b4c020d507e
Sohanur-Rahman642 <rahman.sohanur@northsouth.edu>
2022-05-29 16:42:45 +0600

    feat(redux!) : add the reducer
    Added ImageReducer file as a reducer in order to update state

---
+++ photobooth/redux/reducers/reducers.js
@@ -0,0 +1,63 @@
+import { combineReducers } from "redux";
+import { API_REQUEST, API_SUCCESS, API_LIST_END, API_FAILURE, SET_PAGES } from "../actions/types";
+
+const initialState = {
+    loading: false,
+    moreToLoad: false,
+    error: null,
+    moreError: null,
+    data: [],
+    isListEnd: false,
+}
+
+
+const ImageReducers = (state = initialState, action) => {
+   
+    switch(action.type){
+        case API_REQUEST:
+            console.log("request ")
+            if (action.payload.page === 1) {
+                return { ...state, loading: true }
+            } else {
+                return { ...state, moreToLoad: true }
+            }
+
+        case API_SUCCESS: 
+        console.log("success ")
+ 
+            return {
+                ...state,
+                data: [...state.data, ...action.data],
+                error: null,
+                loading: false,
+                moreToLoad: false
+            }
+
+        case API_FAILURE: 
+        console.log("falilure ", action.error)
+
+            return {
+                ...state,
+                error: action.error,
+                loading: false,
+                moreToLoad: false
+            }
+
+        case API_LIST_END: 
+        console.log("list end ", action)
+
+            return {
+                ...state,
+                isListEnd: true,
+                loading: false,
+                moreToLoad: false
+            }
+            
+        default: return state;
+    }
+}
+
+
+export const imageReducer = combineReducers({
+    images: ImageReducers
+})
