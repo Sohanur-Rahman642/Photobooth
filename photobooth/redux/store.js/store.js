a67a963dc8c0107d22cbf16b248c2e6aabc3d990
Sohanur-Rahman642 <rahman.sohanur@northsouth.edu>
2022-05-29 17:16:08 +0600

    feat(redux!): add the store
    Created the store with the reducer & middleware using redux-thunk..

---
+++ photobooth/redux/store.js/store.js
@@ -0,0 +1,7 @@
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { imageReducer } from '../reducers/reducers';
+
+const rootReducer = combineReducers({ imageReducer });
+
+export const store = createStore(rootReducer, applyMiddleware(thunk));
